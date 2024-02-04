0104f6e60573f74f24860b85ac2646238a2e621f
Robert Wyszynski <robertointerwento@gmail.com>
2024-02-04 19:34:43 +0100

    dodawanie logowania

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, g
+import sqlite3
+
+app_info = {
+    'db_file': 'C:/Users/Robo/Desktop/Heating pump selector/dane/pompa.db'}
 
 app = Flask(__name__)
 
 
+def get_db():
+
+    if not hasattr(g, 'sqlite_db'):
+        conn = sqlite3.connect(app_info['db_file'])
+        conn.row_factory = sqlite3.Row
+        g.sqlite_db = conn
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+
+    if hasattr(g, 'sqlite3_db'):
+        g.sqlite_db.close()
+
+
 class Rodzajpompy:
 
-    def __init__(self, marka, moc, typ, cena):
+    def __init__(self, marka, moc, typ, cena, zro):
         self.marka = marka
         self.moc = moc
         self.typ = typ
         self.cena = cena
+        self.zro = zro
 
     def __repr__(self):
         return f'Wybrałeś pompę marki {self.marka} typ {self.typ} mocy {self.typ}'
@@ -20,9 +41,12 @@ class Informacje:
         self.pompy = []
 
     def zaladuj(self):
-        self.pompy.append(Rodzajpompy('Rotenso', '3kW', 'Airimi', 17000))
-        self.pompy.append(Rodzajpompy('DeDietrich', 'Elensio', 2.5, 11700))
-        self.pompy.append(Rodzajpompy('Buderus', 'WSW19600', 12, 58000))
+        self.pompy.append(Rodzajpompy(
+            'Rotenso', 'Airimi', 2.5, 17000, 'powietrze'))
+        self.pompy.append(Rodzajpompy(
+            'DeDietrich', 'Elensio', 2.5, 11700, 'powietrze'))
+        self.pompy.append(Rodzajpompy(
+            'Buderus', 'WSW19600', 12, 58000, 'woda'))
 
     def kod(self, kody):
         for pompa in self.pompy:
@@ -83,6 +107,10 @@ def doborpompy():
         if 'powietrze_woda' in request.form:
             powietrze_woda = request.form['powietrze_woda']
 
+        moc = 2.0
+        if 'moc' in request.form:
+            moc = request.form['moc']
+
         moc_pompy = (int(typ_domu) * int(metry_ogrzewane) +
                      int(ilosc_osob)*300)/1000
 
@@ -91,4 +119,9 @@ def doborpompy():
         else:
             czy_woda = "powietrze"
 
-        return render_template('doborpompy_wynik.html', metry=metry, moc_pompy=moc_pompy, czy_woda=czy_woda, zrodlo=zrodlo)
+        db = get_db()
+        sql_command = 'select marka, typ, moc, cena, zro from pompowanie;'
+        cur = db.execute(sql_command)
+        transactions = cur.fetchall()
+
+        return render_template('doborpompy_wynik.html', metry=metry, moc_pompy=moc_pompy, moc=moc, czy_woda=czy_woda, zrodlo=zrodlo, transactions=transactions)
